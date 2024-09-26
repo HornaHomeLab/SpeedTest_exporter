b978be7540c0656b76faa4e035493ec4828e1cd9
Stanisław Horna <stanislawhorna@outlook.com>
2024-09-26 22:25:35 +0200

    refactor getting ints

--- app/src/Utils/Config.py
+++ app/src/Utils/Config.py
@@ -8,7 +8,7 @@ load_dotenv()
 class Config:
 
     __connectivity_check_servers: list[str] = ["1.1.1.1", "8.8.8.8", "8.8.4.4"]
-    
+
     __intervals: dict[str, int] = {
         "SPEEDTEST": 1800,
         "CONNECTIVITY": 15
@@ -29,26 +29,32 @@ class Config:
             servers = Config.__connectivity_check_servers
 
         return servers
-    
+
     @staticmethod
     def get_action_interval(name: str) -> int:
-        name = name.upper()
-        
         return int(
-            os.getenv(
-                "{action_name}_INTERVAL".format(
-                    action_name=name),
-                Config.__intervals[name]
-            )
+            Config.__get_env_value(name,"interval", Config.__intervals)
         )
-    
+
     @staticmethod
     def get_subprocess_timeout(name: str) -> int:
-        name = name.upper()
         return int(
-            os.getenv(
-                "{subprocess_name}_TIMEOUT".format(
-                    subprocess_name=name),
-                Config.__timeouts[name]
-            )
+            Config.__get_env_value(name, "timeout", Config.__timeouts)
         )
+
+    def __get_env_value(value_name: str, value_suffix: str, default_values: dict[str, int]):
+        value_name = value_name.upper()
+        value_suffix = value_suffix.upper()
+
+        value_to_return =  os.getenv(
+            "{name}_{suffix}".format(
+                name=value_name,
+                suffix=value_suffix
+            ),
+            default_values.get(value_name, None)
+        )
+
+        if value_to_return is None:
+            raise Exception(f"{value_name} is not defined")
+        
+        return value_to_return
